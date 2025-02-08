448c400cc67f04c5680ad421bf8946201fc3bb69
zidane-sc <zidankren02@gmail.com>
2025-02-08 20:44:50 +0700

    Modify leftSidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,45 +1,48 @@
-"use client"
+"use client";
 
-import Link from "next/link";
-import { sidebarLinks } from "@/constants";
 import Image from "next/image";
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { SignedIn, SignOutButton, useAuth } from "@clerk/nextjs";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 
-function LeftSidebar() {
+import { sidebarLinks } from "@/constants";
+
+const LeftSidebar = () => {
   const router = useRouter();
   const pathname = usePathname();
+
   const { userId } = useAuth();
 
-  if (!userId) return null;
-
   return (
-    <section className="custom-scrollbar leftsidebar">
-      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+    <section className='custom-scrollbar leftsidebar'>
+      <div className='flex w-full flex-1 flex-col gap-6 px-6'>
         {sidebarLinks.map((link) => {
-          const isActive = (pathname.includes(link.route) && link.route.length > 1) || (pathname === link.route);
-          if (link.route === '/profile') link.route = `${link.route}/${userId}`;
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
 
           return (
-            <Link 
+            <Link
               href={link.route}
               key={link.label}
-              className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
+              className={`leftsidebar_link ${isActive && "bg-primary-500 "}`}
             >
-              <Image 
+              <Image
                 src={link.imgURL}
                 alt={link.label}
                 width={24}
                 height={24}
               />
 
-              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+              <p className='text-light-1 max-lg:hidden'>{link.label}</p>
             </Link>
-          )
+          );
         })}
       </div>
 
-      <div className="mt-10 px-6">
+      <div className='mt-10 px-6'>
         <SignedIn>
           <SignOutButton redirectUrl="/sign-in">
               <div className="flex cursor-pointer gap-4 p-4">
@@ -50,13 +53,13 @@ function LeftSidebar() {
                   height={24}
                 />
 
-                <p className="text-light-2 max-lg:hidden">Logout</p>
-              </div>
-            </SignOutButton>
+              <p className='text-light-2 max-lg:hidden'>Logout</p>
+            </div>
+          </SignOutButton>
         </SignedIn>
       </div>
     </section>
   );
-}
+};
 
 export default LeftSidebar;
